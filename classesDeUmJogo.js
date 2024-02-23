3bec8afb30ba65796a61f6420de671456013fd78
Vitorsigma1 <vitorbuono@yahoo.com.br>
2024-02-23 09:15:03 -0300

    segundo commit certo!

--- classesDeUmJogo.js
+++ classesDeUmJogo.js
@@ -1,1 +1,34 @@
-let vamosLagit
+class dataHero {
+    constructor(name, age, atacHero) {
+        this.name = name
+        this.age = age
+        this.atacHero = atacHero
+    }
+
+    attacking() {
+        for (let hero in this.atacHero) {
+            let [tipe, atac] = this.atacHero[hero]
+            console.log(`O ${tipe} atacou usando ${atac}`)
+        }
+    }
+}
+
+let data = {
+    name: "ShazzanVitor",
+    age: 54,
+    atacHero: {
+        0: ["mago", "magia"],
+        1: ["guerreiro", "espada"],
+        2: ["monge", "artes marciais"],
+        3: ["ninja", "shuriken"]
+    }
+}
+
+function heroAction(data) {
+    let heroi = new dataHero(data.name, data.age, data.atacHero)
+    heroi.attacking()
+}
+
+heroAction(data)
+
+
